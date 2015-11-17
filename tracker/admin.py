75e9dbe9d065023e23f64107cbc1b834ba21e1b6
janejl <jane.hkucs@gmail.com>
2015-11-17 23:24:30 +0800

    admin template is added

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import *
 
-admin.site.register(Question)
+admin.site.register(Manager)
+admin.site.register(Developer)
+admin.site.register(Project)
+admin.site.register(Phase)
+admin.site.register(Iteration)
+admin.site.register(Defect)
