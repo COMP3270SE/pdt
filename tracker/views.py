6e02e74b4fc44ee5f457db87cc0e87ce8475dc73
janejl <jane.hkucs@gmail.com>
2015-11-18 17:28:09 +0800

    add templates for tracker. replace HttpResponse, loader, RequestContext by Render

--- tracker/views.py
+++ tracker/views.py
@@ -1,28 +1,36 @@
-# === view ===
-# A view function is a Python function that takes a Web request and returns a Web response.
+# from django.http import HttpResponse
+# from django.template import RequestContext, loader
+from django.shortcuts import render
 
-from django.http import HttpResponse
 from .models import Project
 
 import datetime
 
 # Each view function takes at least one parameter, called request
-# request: an object that contains info about the current Web request that has triggered this view
 def current_datetime(request, uid):
     now = datetime.datetime.now()
     return HttpResponse("<html><body>It is now %s.</body></html>" % now) 
-    
+
+
 def index(request):
-    latest_project_list = Project.objects.order_by('-pid')[:5]
-    output = ', '.join([p.description for p in latest_project_list])
-    return HttpResponse(output)   
+    project_list = Project.objects.order_by('-pid')
+    context = {'project_list': project_list}
+    return render(request, 'tracker/index.html', context)
+    #template = loader.get_template('tracker/index.html')
+    #context = RequestContext(request, {
+    #    'project_list': project_list,
+    #})
+    #return HttpResponse(template.render(context))
     
+
 def detail(request, uid):
     return HttpResponse("You're looking at user %s." % uid)
+
 
 def results(request, uid):
     response = "You're looking at the results of user %s."
     return HttpResponse(response % uid)
+
 
 def vote(request, uid):
     return HttpResponse("You're voting on user %s." % uid)
