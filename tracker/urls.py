1b3a4d268d162a7147d7223fd1785012dd7cf48d
janejl <jane.hkucs@gmail.com>
2015-11-26 01:12:59 +0800

    add some tips

--- tracker/urls.py
+++ tracker/urls.py
@@ -25,4 +25,8 @@ urlpatterns = [
     url(r'^(?P<id>[0-9]{8})/timing/$', views.timing, name='timing'),
     # http://localhost:8000/tracker/50000000/project/1/summary/
     url(r'^(?P<user_id>[0-9]{8})/project/(?P<project_id>[0-9]+)/summary/$', views.summary, name='summary'),
+    # http://localhost:8000/tracker/login
+    url(r'^login/$', views.login, name='login'),
+    # http://localhost:8000/tracker/50000000/project/1/people/
+    url(r'^(?P<user_id>[0-9]{8})/project/(?P<project_id>[0-9]+)/people/$', views.people, name='people'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, render_to_response
 from django.db.models import Aggregate
 from django.db.models import Count, Sum
+
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
 
 from .models import Project, Developer, Manager
 from .models import Phase, Iteration, Defect
@@ -54,5 +57,26 @@ def timing(request, id):
                   'tracker/timing.html',
                   {'user': developer})
 
+def people(request, user_id, project_id):
+	return render(request, 'tracker/people.html', {})
+
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('tracker/login.html',{'state':state, 'username': username})
 
 
