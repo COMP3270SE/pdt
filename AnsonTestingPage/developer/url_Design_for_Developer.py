0b8b97a0340fee48bb44bdc1b5c45b63a3b29184
AnsonWongCS <AnsonWong@AWMac.local>
2015-11-29 19:15:52 +0800

    Make changes on tested Summary

--- AnsonTestingPage/developer/url_Design_for_Developer.py
+++ AnsonTestingPage/developer/url_Design_for_Developer.py
@@ -10,23 +10,13 @@
 # Arrived Developer Home
 # /(uid)/deveHome/
 
-# Choose the project (We have to get the current iteration information)
-# /(uid)/(pid)/
-
 # We have three links out
 
 # First, click the timer ( Start to work )
 # /(uid)/(pid)/timer/
 
-# First - a, submit the timing 
-# (In this page we can start, pause, submit)( only submit pass the data to Database)
-# /(uid)/(pid)/timingSaved/
-
 # Second, report a defect
 # (uid)/(pid)/defectReport/
-
-# Second - a, submit a defect report
-# (uid)/(pid)/defectReportSaved/
 
 # Third, show the summary (for developer)
 # (uid)/(pid)/deveSummary/
